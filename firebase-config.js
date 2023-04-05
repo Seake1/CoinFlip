1aeab4d015a2d5c027684dce040da55199bbb435
Seake1 <christian.eriksson2@gmail.com>
2023-04-05 13:26:26 +0300

    update

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCyfX64pfAfPewUDdZfNdC-ShcxD5ldOrw",
+  authDomain: "coinflip-f2636.firebaseapp.com",
+  projectId: "coinflip-f2636",
+  storageBucket: "coinflip-f2636.appspot.com",
+  messagingSenderId: "144881821009",
+  appId: "1:144881821009:web:b2accfd39cc7c891961f70",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
